d2e129509af38f6e9e51703c470e61e15c21b212
botocomofr1 <botocomofr1@gmail.com>
2016-06-13 19:33:39 -0700

    sync

--- PodeTrimmer/Picnic.Test/PicnicTest.py
+++ PodeTrimmer/Picnic.Test/PicnicTest.py
@@ -5,6 +5,7 @@ class Test_Picnic(unittest.TestCase):
     def test_selectionSortIntegers(self):
         SelectionSortIntegers.selection_sort( SelectionSortIntegers.random_list())
         self.assertGreater(5,3,"Test Failed")
+        
 
 
 if __name__ == '__main__':
